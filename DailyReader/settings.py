d5d84af318efe57588bcdcefc8ed7d78b6749f9f
unknown <virender1910@virender1910.Nagarro.local>
2014-04-20 14:36:29 +0530

    Configuring for heroku

--- DailyReader/settings.py
+++ DailyReader/settings.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
-# import dj_database_url
+import dj_database_url
 DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
