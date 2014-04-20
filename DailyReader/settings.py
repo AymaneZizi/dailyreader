2d271a5fb3c56ed50500b5f8fbb4e1bc3b28e221
unknown <virender1910@virender1910.Nagarro.local>
2014-04-20 14:33:13 +0530

    Configuring for heroku

--- DailyReader/settings.py
+++ DailyReader/settings.py
@@ -8,9 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 # import dj_database_url
- DATABASES = {}
- DATABASES['default'] =  dj_database_url.config()
- SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+DATABASES = {}
+DATABASES['default'] =  dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
